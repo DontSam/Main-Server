d83c87112c581c37717bb56da584f67618773622
DontSam <84098212+DontSam@users.noreply.github.com>
2021-12-21 21:03:23 +0100

    trdsyt
    test

---
+++ DS-BikeHire/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+files {
+    'stream/hiredock.ytyp',
+}
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/hiredock.ytyp'
